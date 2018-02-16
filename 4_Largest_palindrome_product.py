294f82a020d602a4c76a7f77123a6c88ce32ed47
AKSHAY_PC <akkipatel667@gmail.com>
2018-02-16 07:07:43 -0500

    first test of version control

--- 4_Largest_palindrome_product.py
+++ 4_Largest_palindrome_product.py
@@ -1,17 +1,40 @@
-print("hello")
-for i in range(1,5):
-    if i == 2:
-        continue
-    print(i)
-
-def sample(num1,num2,num3):
-    print(num1,num2,num3)
-
-sample(10,20,30)
+#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+#Find the largest palindrome made from the product of two 3-digit numbers.
 
 
+max_mult=99
+max_numb=max_mult * max_mult
+max_len=len(str(max_mult))
+print(max_numb)
 
 
+def ispalindrome(numb):
+    check_numb=str(numb)
+    status = False
+    length_numb=len(check_numb)
+    k=1
+    while k <= int(length_numb // 2):
+        if check_numb[k-1] == check_numb[length_numb-k]:
+            status=True
+        else:
+            status=False
+            break
+        k += 1
+    return status
 
+if __name__ == "__main__":
+    for i in range(max_numb,1,-1):
+        status2 = False
+        if ispalindrome(i) == True:
+            for j in range(max_mult,1,-1):
+                if i % j == 0:
+                    mul1=str(j)
+                    mul2=str(int(i/j))
+                    if len(mul1) == max_len and len(mul2) == max_len:
+                        print(i,j,int(i/j),"Biggest palindromic number")
+                        status2= True
+                        break
+            if status2 == True: break
+    print("Finish the program")
 
 
