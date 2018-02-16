b67f39e4df087e18236195cd35484b29bd103ad3
AKSHAY_PC <akkipatel667@gmail.com>
2018-02-16 01:22:17 -0500

    first test of version control

---
+++ 3_Largest_prime_factor.py
@@ -0,0 +1,42 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
+number = 13195
+number2= 84
+prime_list = []
+prime_list2=[]
+
+def isprime(x):
+    status = False
+    for y in range(2, x):
+        if x % y == 0:
+            status=False
+            break
+        else:
+            status=True
+    return status
+
+for i in range(2, number): # Traditional approach to search the prime factor
+    if number % i == 0:
+        if isprime(i) == True:
+            prime_list.append(i)
+
+def check_prime(numb):    # New approach which is way faster than the usual one
+    i=2
+    while numb>1:
+        if numb % i == 0:
+            prime_list2.append(i)
+            numb=int(numb/i)
+            while True:
+                if numb%i != 0:
+                    break
+                else:
+                    numb=numb/i
+                    prime_list2.append(i)
+        i = i+1
+
+check_prime(number2)
+print(prime_list)
+print(max(prime_list))
+print(prime_list2)
+print(max(prime_list2))
---
+++ 4_Largest_palindrome_product.py
@@ -0,0 +1,17 @@
+print("hello")
+for i in range(1,5):
+    if i == 2:
+        continue
+    print(i)
+
+def sample(num1,num2,num3):
+    print(num1,num2,num3)
+
+sample(10,20,30)
+
+poly morphism
+
+
+
+
+
