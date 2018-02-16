3a94744efb4111822b220ff46d39e52ae481de9e
AKSHAY_PC <akkipatel667@gmail.com>
2018-02-16 08:56:54 -0500

    added comments

--- 3_Largest_prime_factor.py
+++ 3_Largest_prime_factor.py
@@ -1,8 +1,8 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
-
+# In Below code, I have used two approach to get the largest prime factor
 number = 13195
-number2= 84
+number2= 600851475143
 prime_list = []
 prime_list2=[]
 
