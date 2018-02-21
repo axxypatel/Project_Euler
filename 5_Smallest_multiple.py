6450e7b178c0699c716bcd8ecc3efae9de526c3c
AKSHAY_PC <akkipatel667@gmail.com>
2018-02-21 10:12:03 -0500

    changes done for range function which include till_numb+1 login in second parameter of range function

--- 5_Smallest_multiple.py
+++ 5_Smallest_multiple.py
@@ -1,12 +1,13 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 final_numb=1
-for i in range (2,20):
+till_numb= 20
+for i in range (2,till_numb+1):
     if final_numb % i == 0:
         continue
     numb = i
 
-    while numb <=20:
+    while numb <=till_numb:
         temp= numb
         numb = numb * i
 
@@ -14,4 +15,5 @@ for i in range (2,20):
 
 print("final number",final_numb)
 print("program completed")
+
 
