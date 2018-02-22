4e9c62288a02c7dc46140306656b8772e9466655
AKSHAY_PC <akkipatel667@gmail.com>
2018-02-22 09:32:41 -0500

    final code using simple primality test algorithm to test prime number

--- 3_Largest_prime_factor.py
+++ 3_Largest_prime_factor.py
@@ -1,20 +1,30 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 # In Below code, I have used two approach to get the largest prime factor
+
+import math
 number = 13195
 number2= 600851475143
 prime_list = []
-prime_list2=[]
+prime_list2= []
 
-def isprime(x):
-    status = False
-    for y in range(2, x):
-        if x % y == 0:
-            status=False
-            break
-        else:
-            status=True
-    return status
+
+def isprime(x): # simple primality test algorithm implemented
+    if x <= 1:
+        return False
+    if x == 2 or x == 3:
+        return True
+    if x % 2 == 0 or x % 3 ==0:
+        return False
+
+    i = 5
+    sqrt_numb = int(math.sqrt(x))
+
+    while i <= sqrt_numb: # check divisibility till the square root of the number
+        if x % i == 0:
+            return False
+        i += 2
+    return True
 
 for i in range(2, number): # Traditional approach to search the prime factor
     if number % i == 0:
