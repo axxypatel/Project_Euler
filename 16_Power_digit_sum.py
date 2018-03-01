d66278bf0c0acebe0cabd18eb609125e7745c059
AKSHAY_PC <akkipatel667@gmail.com>
2018-03-01 05:15:13 -0500

    simple code to implement

---
+++ 16_Power_digit_sum.py
@@ -0,0 +1,17 @@
+#2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#What is the sum of the digits of the number 2^1000?
+
+sum = 0
+numb = 1000
+tot_mul = 1
+
+for i in range(1,numb+1):
+    tot_mul *= 2
+
+tot_mul = str(tot_mul)
+#print(tot_mul)
+for j in range(0,len(tot_mul)):
+    sum += int(tot_mul[j])
+
+print("Total sum:",sum)
+
