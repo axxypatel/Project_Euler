246140de451e4e3308adf44f9352238b07a455e9
AKSHAY_PC <akkipatel667@gmail.com>
2018-03-08 07:35:19 -0500

    Reformat the file

--- 3_Largest_prime_factor.py
+++ 3_Largest_prime_factor.py
@@ -4,46 +4,49 @@
 
 import math
 number = 13195
-number2= 600851475143
+number2 = 600851475143
 prime_list = []
-prime_list2= []
+prime_list2 = []
 
 
-def isprime(x): # simple primality test algorithm implemented
+def is_prime(x):  # simple primality test algorithm implemented
     if x <= 1:
         return False
     if x == 2 or x == 3:
         return True
-    if x % 2 == 0 or x % 3 ==0:
+    if x % 2 == 0 or x % 3 == 0:
         return False
 
-    i = 5
-    sqrt_numb = int(math.sqrt(x))
+    y = 5
+    sqt_numb = int(math.sqrt(x))
 
-    while i <= sqrt_numb: # check divisibility till the square root of the number
-        if x % i == 0:
+    while y <= sqt_numb:  # check divisibility till the square root of the number
+        if x % y == 0:
             return False
-        i += 2
+        y += 2
     return True
 
-for i in range(2, number): # Traditional approach to search the prime factor
+
+for i in range(2, number):  # Traditional approach to search the prime factor
     if number % i == 0:
-        if isprime(i) == True:
+        if is_prime(i):
             prime_list.append(i)
 
+
 def check_prime(numb):    # New approach which is way faster than the usual one
-    i=2
-    while numb>1:
-        if numb % i == 0:
-            prime_list2.append(i)
-            numb=int(numb/i)
+    j = 2
+    while numb > 1:
+        if numb % j == 0:
+            prime_list2.append(j)
+            numb = int(numb/j)
             while True:
-                if numb%i != 0:
+                if numb % j != 0:
                     break
                 else:
-                    numb=numb/i
-                    prime_list2.append(i)
-        i = i+1
+                    numb = numb/j
+                    prime_list2.append(j)
+        j += 1
+
 
 check_prime(number2)
 print(prime_list)
