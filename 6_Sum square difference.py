b6e76f30faedde637818e05ffe04f5eaff2df84e
AKSHAY_PC <akkipatel667@gmail.com>
2018-03-20 08:28:05 -0400

    Implement the code using class and object

--- 6_Sum square difference.py
+++ 6_Sum square difference.py
@@ -1,28 +1,34 @@
-#The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
-#The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 552 = 3025
-#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+# The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
+# The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
-#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 till_numb = 100
 
-def sum_sqr_numb(numb):
-    sum=0
-    for i in range(1,numb+1):
-        sum = sum + (i*i)
 
-    return sum
+class SquareDiff:
 
-def sqr_sum_numb(numb):
-    sum=0
-    for i in range(1,numb+1):
-        sum=sum+i
-    return sum*sum
+    def __init__(self, till_numb1):
+        self.numb = till_numb1
+
+    def sum_sqr_numb(self):
+        total_sum = 0
+        for i in range(1, self.numb+1):
+            total_sum = total_sum + (i*i)
+
+        return total_sum
+
+    def sqr_sum_numb(self):
+        total_sum = 0
+        for i in range(1, self.numb+1):
+            total_sum = total_sum + i
+        return total_sum * total_sum
 
 
-numb1=sum_sqr_numb(till_numb)
+sample_obj = SquareDiff(till_numb)
+numb1 = sample_obj.sum_sqr_numb()
 print(numb1)
-numb2=sqr_sum_numb(till_numb)
+numb2 = sample_obj.sqr_sum_numb()
 print(numb2)
-
 print(numb2-numb1)
