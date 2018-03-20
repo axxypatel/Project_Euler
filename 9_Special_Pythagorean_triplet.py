d1e69a2615bb480d3aff7d6a095b9f6f1c29df4a
AKSHAY_PC <akkipatel667@gmail.com>
2018-03-20 08:39:47 -0400

    Updated the code

--- 9_Special_Pythagorean_triplet.py
+++ 9_Special_Pythagorean_triplet.py
@@ -1,30 +1,28 @@
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,a^2 + b^2 = c^2. For example, 32 + 42 = 9 + 16 = 25 = 52.
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
-
 import math
-a=2
-b=3
-c=1
 
-status=False
-for i in range(1,999):
-    for j in range(i+1,1000):
-        sum=0
+a = 2
+b = 3
+c = 1
+
+status = False
+for i in range(1, 999):
+    c_sqr = 0
+    j = 0
+    for j in range(i+1, 1000):
+        total_sum = 0
         c_sqr = (i * i) + (j*j)
-        #print(c_sqr)
-        if math.sqrt(c_sqr).is_integer() == False:
+
+        if math.sqrt(c_sqr).is_integer() is False:
             continue
         else:
-            sum = i + j+ int(math.sqrt(c_sqr))
-            #print("Sum:",i,j,int(math.sqrt(c_sqr)),sum)
-            if sum == 1000:
+            total_sum = i + j + int(math.sqrt(c_sqr))
+            if total_sum == 1000:
                 status = True
                 break
-    if status == True:
-        a,b,c = i,j,int(math.sqrt(c_sqr))
-        #print(a*b*c)
+    if status is True:
+        a, b, c = i, j, int(math.sqrt(c_sqr))
         break
 
-print("Sum and Product of a,c and c: ",a+b+c,a*b*c)
-
-
+print("Sum and Product of a,c and c: ", a+b+c, a*b*c)
