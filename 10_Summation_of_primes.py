5e1bcc83c2481d5ff8162d4f5f681e83b909876d
AKSHAY_PC <akkipatel667@gmail.com>
2018-03-20 08:42:48 -0400

    Updated the code

--- 10_Summation_of_primes.py
+++ 10_Summation_of_primes.py
@@ -1,12 +1,12 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
-#2000000
-
-
+# 2000000
 import math
-below_numb= 2000000
 
-def is_prime(numb): # use simple primality test algorithm having O(sqrt(N)) time complexity
+below_numb = 2000000
+
+
+def is_prime(numb):  # use simple primality test algorithm having O(sqrt(N)) time complexity
 
     if numb == 2 or numb == 3:
         return True
@@ -15,22 +15,23 @@ def is_prime(numb): # use simple primality test algorithm having O(sqrt(N)) time
     if numb % 3 == 0:
         return False
     i = 5
-    sqrt_numb = int(math.sqrt(numb))
+    srt_numb = int(math.sqrt(numb))
 
-    while i <= sqrt_numb: # Check till square root of number and test the divisibity
+    while i <= srt_numb:  # Check till square root of number and test the divisibity
         if numb % i == 0:
             return False
         i += 2
     return True
 
 
-def calc_sum(below_numb):
-    sum=0
-    for i in range(2,below_numb):
-        if is_prime(i) == True:
-            sum = sum+i
+def calc_sum(below_numb1):
+    total_sum = 0
+    for i in range(2, below_numb1):
+        if is_prime(i) is True:
+            total_sum = total_sum + i
 
-    return sum
+    return total_sum
 
-sum= calc_sum(below_numb)
-print("Sum:",sum)
+
+total_sum1 = calc_sum(below_numb)
+print("Sum:", total_sum1)
