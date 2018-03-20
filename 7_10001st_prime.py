33dd6b89e40b655c6d56010593c2d9a6b26f61cb
AKSHAY_PC <akkipatel667@gmail.com>
2018-03-20 08:32:45 -0400

    Updated the code

--- 7_10001st_prime.py
+++ 7_10001st_prime.py
@@ -1,6 +1,5 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10001st prime number? 104743
-
 import random
 import math
 
@@ -8,8 +7,7 @@ prime_pos = 10001
 
 
 # Fermat method algorithm implemented to check the prime number
-
-def power(a , n , k):
+def power(a, n, k):
     res = 1
     a = a % k
 
@@ -23,14 +21,16 @@ def power(a , n , k):
 
 
 def is_prime_fermat(numb):  # This method is slow in this problem
-    if numb <= 1 or numb == 4: return False
-    if numb <= 3: return True
+    if numb <= 1 or numb == 4:
+        return False
+    if numb <= 3:
+        return True
 
     k = 3
 
     while k > 0:
-        a = 2 + random.randint(2,numb-2) % (numb - 4)
-        if power( a , numb - 1 , numb ) != 1:
+        a = 2 + random.randint(2, numb-2) % (numb - 4)
+        if power(a, numb - 1, numb) != 1:
             return False
         k -= 1
     # print("Prime:",numb)
@@ -38,8 +38,7 @@ def is_prime_fermat(numb):  # This method is slow in this problem
 
 
 # simple primality test algorithm implemented
-
-def isprime_primality(x):  # This code is fast in current problem
+def is_prime_primal(x):  # This code is fast in current problem
     if x <= 1:
         return False
     if x == 2 or x == 3:
@@ -48,26 +47,26 @@ def isprime_primality(x):  # This code is fast in current problem
         return False
 
     i = 5
-    sqrt_numb = int( math.sqrt( x ) )
+    srt_numb = int(math.sqrt(x))
 
-    while i <= sqrt_numb:  # check divisibility till the square root of the number
+    while i <= srt_numb:  # check divisibility till the square root of the number
         if x % i == 0:
             return False
         i += 2
     return True
 
 
-def check_cond(prime_pos):
+def check_cond(prime_pos1):
     numb_position = 0
     j = 2
     while True:
-        if isprime_primality(j):
+        if is_prime_primal(j):
             numb_position += 1
             # print(numb_position)
-            if numb_position == prime_pos:
-                print( "Position of the prine number:" , numb_position , "\n" , "Prime Number:" , j )
+            if numb_position == prime_pos1:
+                print("Position of the prime number:", numb_position, "\n", "Prime Number:", j)
                 break
         j += 1
 
 
-check_cond( prime_pos )
+check_cond(prime_pos)
