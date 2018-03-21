c6a79ff0370f5718cd6d53141ede9eeb8d17d6d4
AKSHAY_PC <akkipatel667@gmail.com>
2018-03-21 07:50:52 -0400

    Updated the code- Import module only in function definition

--- 3_Largest_prime_factor.py
+++ 3_Largest_prime_factor.py
@@ -1,7 +1,6 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 # In Below code, I have used two approach to get the largest prime factor
-import math
 
 
 class PrimeFactorMethod:
@@ -13,6 +12,7 @@ class PrimeFactorMethod:
         self.prime_list2 = []
 
     def is_prime(self, x):  # simple primality test algorithm implemented
+        import math
         if x <= 1:
             return False
         if x == 2 or x == 3:
