8366c77499073d59d80de2ac679d4663d112b0bf
AKSHAY_PC <akkipatel667@gmail.com>
2018-03-27 09:13:34 -0400

    Stack implementation using list collections in python along with the parentheses balancing example

--- stack_implementation_using_python_list.py
+++ stack_implementation_using_python_list.py
@@ -21,21 +21,48 @@ class Stack:
         return len(self.item_list)
 
 
-sample_object = Stack()
+def test_stack_class():
+    sample_object = Stack()
+    sample_object.push(4)
+    sample_object.push('dog')
+    sample_object.push(4.7777)
+    print("Added the value:", sample_object.item_list)
 
-sample_object.push(4)
-sample_object.push('dog')
-sample_object.push(4.7777)
-print("Added the value:", sample_object.item_list)
+    sample_object.pop()
+    print("Remove the top value:", sample_object.item_list)
 
-sample_object.pop()
-print("Remove the top value:", sample_object.item_list)
+    print("Show the top value:", sample_object.peek())
 
-print("Show the top value:", sample_object.peek())
+    print("Size the stack:", sample_object.size())
 
-print("Size the stack:", sample_object.size())
+    if sample_object.isempty():
+        print("Stack is empty")
+    else:
+        print("Stack is not empty")
 
-if sample_object.isempty():
-    print("Stack is empty")
-else:
-    print("Stack is not empty")
+
+def check_balanced_parentheses(parentheses_string):
+    test_object = Stack()
+    string_len = len(parentheses_string)
+    status = True
+    for temp in range(0, string_len):
+        temp_char = parentheses_string[temp]
+        if temp_char == "(":
+            test_object.push(temp_char)
+        else:
+            if test_object.isempty():
+                status = False
+            else:
+                test_object.pop()
+    if test_object.isempty() and status:
+        return True
+    else:
+        return False
+
+
+print(check_balanced_parentheses('((()))'))
+print(check_balanced_parentheses('(()))'))
+
+
+
+
