7b9f41239a8d2269f8bffdf4b11913d9e727d60a
AKSHAY_PC <akkipatel667@gmail.com>
2018-04-11 09:01:45 -0400

    Updated the primality test algorithm

--- 27_Quadratic_primes.py
+++ 27_Quadratic_primes.py
@@ -8,7 +8,6 @@
 # where |n| is the modulus/absolute value of n e.g. |11|=11 and |−4|=4
 
 # Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
-import math
 import time
 start_time = time.time()
 
